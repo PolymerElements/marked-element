3fcaf6fb48424b2f7a4c3da129a6020cfea414e0
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:38 -0700

    auto-converted by polymer-modulizer

--- marked-import.js
+++ marked-import.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,13 +6,12 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<script src="../marked/lib/marked.js"></script>
-<script>
-  if (!window.marked) {
-    // For webpack support for the Polymer 3 version created by the Polymer
-    // Modulizer More info:
-    // https://github.com/PolymerElements/marked-element/issues/81
-    window.marked = require('../../marked/lib/marked.js');
-  }
-</script>
+*/
+import 'marked/lib/marked.js';
+
+if (!window.marked) {
+  // For webpack support for the Polymer 3 version created by the Polymer
+  // Modulizer More info:
+  // https://github.com/PolymerElements/marked-element/issues/81
+  window.marked = require('../../marked/lib/marked.js');
+}
